d80310d991ac946608f87ea5dcf971f0e26fd4ed
talgat55 <lightxdesign55@gmail.com>
2019-11-24 12:24:47 +0600

    add foter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from  '../components/Header';
 import TabComponent from '../components/Tab';
+import Footer from '../components/Footer';
 class Main extends React.Component {
     render() {
         return (
             <>
                 <Header/>
                 <TabComponent/>
+                <Footer/>
             </>
         );
     }
